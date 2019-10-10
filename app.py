70857ddcf442658a44c22ca63382e319c59f7782
ohld <>
2019-10-10 16:36:05 +0300

    return json

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # INSPIRED WITH: 
 # https://medium.com/@zwork101/making-a-flask-proxy-server-online-in-10-lines-of-code-44b8721bca6
 
-from flask import Flask
+from flask import Flask, Response
 from requests import get
 
 app = Flask(__name__)
@@ -10,7 +10,11 @@ SITE_NAME = 'http://socket.gramup.me/'
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def proxy(path):
-  return get(f'{SITE_NAME}{path}').content
+    return Response(
+        get(f'{SITE_NAME}{path}').content, 
+        status=200, 
+        mimetype='application/json'
+)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
